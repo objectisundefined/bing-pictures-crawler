40c5e840d2004633e145b59396da787bb28a8867
objectisundefined <2535612040@qq.com>
2022-04-11 19:38:20 +0800

    feat: write image url only

--- src/main/scala/PullT.scala
+++ src/main/scala/PullT.scala
@@ -3,13 +3,20 @@ import cats.syntax.all._
 import java.io.{File, InputStream, OutputStream, FileInputStream, FileOutputStream, ByteArrayInputStream}
 import scala.io._
 
+/* Config */
+object PullC {
+  val target = "./src/main/resources/db/images.yaml"
+}
+
+/* Model */
 object PullM {
   final case class Image(copyright: String, startdate: String, enddate: String, url: String)
 
   final case class Res(images: List[Image])
 }
 
-object PullR {
+/* Fetcher */
+object PullF {
   import PullM._
 
   // https://circe.github.io/circe/
@@ -21,54 +28,61 @@ object PullR {
 
   val backend = HttpURLConnectionBackend()
 
-  def run = IO {
+  def fromEither[A](e: Either[Throwable, A]): IO[A] = e.fold(IO.raiseError, IO.pure)
+
+  val fill = (x: Image) => x.copy(url = s"https://cn.bing.com${x.url}")
+
+  val run = (n: Int, size: Int) => IO {
     val response: Identity[Response[Either[ResponseException[String, io.circe.Error], Res]]] =
       basicRequest
-        .get(uri"http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=100&mkt=zh-CN")
+        .get(uri"https://cn.bing.com/HPImageArchive.aspx?format=js&idx=${n}&n=${size}&mkt=zh-CN")
         .response(asJson[Res])
         .send(backend)
 
-    response.body.map(_.images)
-  }
+    response.body.map(_.images.map(fill).reverse)
+  } flatMap fromEither
 }
 
-object PullW {
-  import scala.concurrent.ExecutionContext
-  import scala.concurrent.duration._
+/* Reader */
+object PullR {
+  import java.io._
 
-  // https://sttp.softwaremill.com/en/latest/examples.html
-  import sttp.client3._
-  import sttp.model._
-
-  def arrayToTuple[T] = (l: Array[T]) => (l(0), l(1))
-
-  val backend = HttpURLConnectionBackend()
-
-  def run(url: String) = IO {
-    // eg: /th?id=OHR.FortedeSao_ZH-CN0093358703_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp
-    val segments = url.split("\\?")
-
-    val path = segments(0)
-    val params = segments(1).split("&").map(s => arrayToTuple[String](s.split("="))).toMap
-
-    val name = params.get("id").map(_.substring(4)).getOrElse("default")
-
-    val uri = Uri(host = "cn.bing.com").withWholePath(path).withParams(params)
-
-    basicRequest
-      .get(uri)
-      .response(asFile(new File(s"./src/main/resources/db/${name}")))
-      .send(backend)
+  val acquire: IO[BufferedReader] = IO {
+    new BufferedReader(new FileReader(new File(PullC.target)))
   }
+
+  val release: BufferedReader => IO[Unit] = br => IO { br.close } *> IO(())
+
+  val loop: (BufferedReader, List[String], Int) => IO[List[String]] = (in, acc, range) =>
+    IO(in.readLine()).flatMap[List[String]] { line =>
+      if (line != null) {
+        loop(in, line +: acc.take(range - 1), range)
+      } else {
+        IO(acc)
+      }
+    }
+
+  val run = (range: Int) => Resource.make(acquire)(release).use(br => loop(br, List[String](), range))
+}
+
+/* Writer */
+object PullW {
+  import java.io._
+
+  val acquire: IO[BufferedWriter] = IO {
+    new BufferedWriter(new FileWriter(new File(PullC.target), true))
+  }
+
+  val release: BufferedWriter => IO[Unit] = bw => IO { bw.close } *> IO(())
+
+  val run = (lines: List[String]) => Resource.make(acquire)(release).use(bw => lines.map(line => IO { bw.write(s"${line}\n") }).sequence.void)
 }
 
 object PullT extends IOApp {
-  def fromEither[A](e: Either[Throwable, A]): IO[A] = e.fold(IO.raiseError, IO.pure)
-
   override def run(args: List[String]): cats.effect.IO[cats.effect.ExitCode] =
     for {
-      imagesE <- PullR.run
-      images <- fromEither(imagesE)
-      _ <- images.map(i => PullW.run(i.url)).parSequence.void
+      tail <- PullR.run(7)
+      images <- PullF.run(0, 7)
+      _ <- PullW.run(images.map(x => s"- ${x.url}").filter(!tail.contains(_)))
     } yield ExitCode.Success
 }
